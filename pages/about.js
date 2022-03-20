6411a9ebd308ab2be70dcbefdeaf7a22b7b40016
MDaud32 <theprofessor543@gmail.com>
2022-03-20 22:26:58 +0500

    adding content

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ const About = () => {
       {/* logo arrowicon social media icon */}
       <div className="flex lg:w-[80%] p-6 mx-auto flex-row justify-between items-center text-center py-12">
         <div className=" flex lg:flex-row flex-col gap-7 items-center">
-          <div className="lg:text-4xl text-5xl text-white mt-4 self-start">
+          <div className="lg:text-4xl text-5xl text-white mt-2 self-start">
             <Link href="/">
               <a>
                 <FiArrowUpCircle className="" />
@@ -20,7 +20,7 @@ const About = () => {
             SANDIIP PORWAL
           </h1>
         </div>
-        <div className="self-start ml-[-2rem]">
+        <div className="self-start ml-[-12rem]">
           <SocialMediaIcons />
         </div>
       </div>
@@ -28,7 +28,7 @@ const About = () => {
       {/* content */}
       <div className="lg:w-[80%]  w-[90%] mx-auto my-auto flex flex-col justify-center items-center text-left">
         <div className=" text-white font-semibold my-20">
-          <p className="text-5xl">About Degital Marketing Expert</p>
+          <p className="lg:text-5xl text-3xl">About Degital Marketing Expert</p>
         </div>
         {/* <div className="justify-end self-end text-right">
           <p className="lg:mt-[-4rem] mt-[-3rem]  border-t-2 text-gray-300">
@@ -37,13 +37,16 @@ const About = () => {
         </div> */}
         <div className="flex w-[80%] flex-col md:flex-row lg:flex-row text-white text-2xl mt-5 gap-4">
           <p>
-            About Sandiip Porwal Digital Marketing Consultancy The mission is
-            simple and clear. To demystify & simplify the digital marketing to
-            help our clients scale their business with digital branding, content
-            marketing, digital design and tech expertise that ensure last mile
-            delivery. Essentially we help businesses increase awareness, and
-            revenue by utilizing our digital marketing expertise and a team that
-            has a clear sight of our clients business goals.
+            <span className="lg:text-[1.8rem] text-2xl text-gray-200">
+              About Sandiip Porwal Digital Marketing Consultancy
+            </span>
+            <br /> <br />
+            The mission is simple and clear. To demystify & simplify the digital
+            marketing to help our clients scale their business with digital
+            branding, content marketing, digital design and tech expertise that
+            ensure last mile delivery. Essentially we help businesses increase
+            awareness, and revenue by utilizing our digital marketing expertise
+            and a team that has a clear sight of our clients business goals.
           </p>
         </div>
         <button className="px-6 py-4 bg-yellow-500 mt-5 rounded-md text-xl font-semibold cursor-pointer">
--- pages/contact.js
+++ pages/contact.js
@@ -20,22 +20,22 @@ const Contact = () => {
             SANDIIP PORWAL
           </h1>
         </div>
-        <div className="self-start ml-[-4rem]">
+        <div className="self-start ml-[-10rem]">
           <SocialMediaIcons />
         </div>
       </div>
 
       {/* content */}
       <div className="lg:w-[80%]  w-[90%] mx-auto my-auto flex flex-col justify-center items-center text-left">
-        <div className=" text-white  flex text-left font-semibold mt-10">
-          <p className="text-5xl justify-start">
+        <div className=" text-white flex text-left font-semibold mt-10">
+          <p className="lg:text-5xl text-3xl justify-start">
             Want to Grow your Business with better Digital Marketing Results.
           </p>
         </div>
         <button className="px-6 py-4 bg-yellow-500 my-8 rounded-md text-xl font-semibold cursor-pointer underline">
           We Will Help Your Get There.
         </button>
-        <div className=" text-white font-semibold my-10">
+        <div className=" text-white font-semibold lg:my-10 my-7 text-center">
           <p className="text-5xl">Schedule Appointment</p>
         </div>
         {/* <div className="justify-end self-end text-right">
@@ -43,17 +43,25 @@ const Contact = () => {
             Mark Nikson, General director
           </p>
         </div> */}
-        <div className="flex w-[80%] flex-col md:flex-row lg:flex-row text-white text-2xl mt-5 gap-4">
+        <div className="flex w-[80%] flex-col md:flex-row lg:flex-row text-white text-lg lg:text-2xl lg:mt-5 mt-3">
           <p>
             Whether it is slow revenue or you are unsure where to start with
             digital marketing or how to re-energise your digital marketing?
+            <br />
+            <br />
             Whatever your requirements or budget, our talented team of digital
             experts at Sandiip Porwal Digital Marketing Consultancy is here and
-            ready to help you. We offer a first free consultation followed by a
-            brainstorming session pre-signup for a fee and then once you sign up
-            for our digital marketing services you have with you a
-            performance-driven digital agency as marketing partner Schedule an
-            appointment and lets take your business to the next level!
+            ready to help you.
+            <br />
+            <br />
+            We offer a first free consultation followed by a brainstorming
+            session pre-signup for a fee and then once you sign up for our
+            digital marketing services you have with you a performance-driven
+            digital agency as marketing partner.
+            <br />
+            <br />
+            Schedule an appointment and lets take your business to the next
+            level!
           </p>
         </div>
       </div>
