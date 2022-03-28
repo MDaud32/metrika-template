6d587e35df3a0bee1af2bb08619dfc41b4cc5f4a
MDaud32 <theprofessor543@gmail.com>
2022-03-28 14:59:29 +0500

    adding styles to collaboration page

--- pages/collaboration.js
+++ pages/collaboration.js
@@ -1,42 +1,49 @@
 import React from 'react';
 import SocialMediaIcons from '../components/socialMediaIcons';
-import { FiArrowUpCircle } from 'react-icons/fi';
+import { AiFillSignal } from 'react-icons/ai';
 import Link from 'next/link';
-import Footer from '../components/footer';
+import PageChange from '../components/pageChange';
 
 const Collaborate = () => {
   return (
-    <div className="w-screen h-screen flex flex-col  bg-[#d9912a]">
-      {/* logo arrowicon social media icon */}
-      <div className="flex lg:w-[80%] w-full p-6 mx-auto flex-row justify-between items-center text-center pt-12">
-        <div className=" flex lg:flex-row flex-col gap-4 items-center">
-          <div className="lg:text-4xl text-5xl text-white mt-2 self-start">
+    <div className=" flex flex-col lg:flex-row ">
+      <div className="lg:w-1/2 lg:h-screen text-black bg-gray-400 p-10">
+        <div className="flex flex-row gap-1 lg:gap-[3rem] justify-center items-center ">
+          <div className="lg:text-3xl text-3xl lg:mr-[-8.5rem] lg:ml-[2rem] text-white justify-end mt-1">
             <Link href="/">
               <a>
-                <FiArrowUpCircle className="lg:mt-2 " />
+                <PageChange target={'/about'} />
               </a>
             </Link>
           </div>
-          <h1 className="lg:text-6xl text-left md:text-6xl text-4xl text-white">
+          <h1 className="lg:text-3xl text-3xl text-center justify-center md:text-left text-white lg:mx-auto whitespace-nowrap">
             SANDIIP PORWAL
           </h1>
         </div>
-        <div className="self-start lg:ml-[-2rem] ml-[-12rem]">
-          <SocialMediaIcons />
+        <p className="lg:text-4xl text-3xl md:justify-center lg:justify-center font-semibold lg:my-28 my-14 lg:ml-16  lg:w-[25rem] flex md:text-left text-center ml-6mx-auto">
+          Collaboration With Me
+        </p>
+        <div>
+          <AiFillSignal size={80} color={'black'} className={'mx-auto'} />
         </div>
       </div>
-
-      {/* content */}
-      <div className="mx-auto my-auto flex flex-col justify-center items-center text-left mt-20 lg:mt-40 gap-10">
-        <h1 className="text-3xl lg:text-6xl text-white font-semibold underline">
-          Collaborate With Me
-        </h1>
-        <button className="px-6 py-4 bg-violet-800 text-white my-12 rounded-md text-xl font-semibold cursor-pointer ">
-          We Will Help Your Get There.
-        </button>
-      </div>
-      <div className="flex flex-col justify-end ">
-        <Footer />
+      {/* <div className="justify-end self-end text-right">
+          <p className="lg:mt-[-4rem] mt-[-3rem]  border-t-2 text-gray-300">
+            Mark Nikson, General director
+          </p>
+        </div> */}
+      <div>
+        <div className="flex justify-end">
+          <SocialMediaIcons />
+        </div>
+        <div className="mx-auto my-auto flex flex-col justify-center items-center text-left mt-20 lg:mt-40 gap-10">
+          <h1 className="text-3xl lg:text-6xl text-white font-semibold underline">
+            Collaborate With Me
+          </h1>
+          <button className="px-6 py-4 bg-violet-800 text-white my-12 rounded-md text-xl font-semibold cursor-pointer underline lg:ml-10">
+            We Will Help Your Get There.
+          </button>
+        </div>
       </div>
     </div>
   );
